fdf6ed1a96f923b323f85a115b3238357f1aa753
jhiy2004 <jhiy2004@gmail.com>
2025-07-30 19:11:12 -0300

    Adicionado imagens binarizadas e processadas

---
+++ process_images.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import os
+import numpy as np
+
+def proccess_image(img_path, output_folder):
+    img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
+
+    img = cv.bitwise_not(img)
+
+    blur = cv.GaussianBlur(img, (5, 5), 0)
+    equalized = cv.equalizeHist(blur)
+
+    _, thresh = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+
+    kernel = np.ones((5, 5), np.uint8)
+    dilate = cv.dilate(thresh, kernel, iterations=2)
+
+    filename = os.path.basename(img_path)
+    output_path = os.path.join(output_folder, filename)
+
+    cv.imwrite(output_path, dilate)
+
+def process_images():
+    output_folder = 'output/'
+    train_folder = 'train/'
+    val_folder = 'val/'
+
+    os.makedirs(os.path.join(output_folder, train_folder), exist_ok=True)
+    os.makedirs(os.path.join(output_folder, val_folder), exist_ok=True)
+
+    list_dirs = os.listdir(train_folder)
+    for diretorio in list_dirs:
+        char_folder = os.path.join(train_folder, diretorio)
+        output_char_folder = os.path.join(output_folder, char_folder)
+        os.makedirs(output_char_folder, exist_ok=True)
+
+        list_arquivos = os.listdir(char_folder)
+        for arquivo in list_arquivos:
+            input_path = os.path.join(char_folder, arquivo)
+            output_path = os.path.join(output_folder, char_folder)
+            proccess_image(input_path, output_path)
+
+process_images()
